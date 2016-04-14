bbdbaa3956990df2afabaeec928973a19bfd059d
James Chu <james@james-chu.com>
2016-04-13 22:29:07 -0700

    Convert filtering to MixItUp

--- js/combine.js
+++ js/combine.js
@@ -1,16 +1,10 @@
 $(function(){
     $(window).load(function(){
-
-        var $container = $('.work-grid').isotope({
-            itemSelector: '.work-item',
+      $('#work-grid').mixItUp({
+        selectors: {
+          target: '.work-item',
+          filter: '.subnav-btn'
+        }
         });
-
-        $('.subnav-content .subnav-item a').click(function() {
-            $(this).find('active').removeClass('active');
-            $(this).addClass('active');
-            var filterValue = $(this).attr('data-filter');
-            $container.isotope({ filter: filterValue });
-        });
-
     });
 });
