dfc922f41705f61508e95bde2901cb2de6ea7b3d
James Chu <james@james-chu.com>
2016-06-24 23:00:50 -0700

    Fixed Isotope to be jQuery independent

--- js/combine.js
+++ js/combine.js
@@ -16,43 +16,37 @@
 //         });
 //     });
 // });
-// NOT WORKING OMFGAJFLDJFJLAFDJLALSJDAJL
-var iso = new Isotope( '.work-content', {
-  itemSelector: '.work-item',
-  layoutMode: 'fitRows'
+var iso = new Isotope('.work-content', {
+    itemSelector: '.work-item',
+    layoutMode: 'fitRows'
 });
 
 var filtersElem = document.querySelectorAll('.jc-ln-item');
-for (var i = 0; i < filtersElem.length; i ++) {
-  var current = filtersElem[i];
-  current.addEventListener( 'click', function( event ) {
-    var filterValue = event.target.getAttribute('data-filter');
-    // use matching filter function
-    iso.arrange({ filter: filterValue });
+for (var i = 0; i < filtersElem.length; i++) {
+    var filterGroup = filtersElem[i];
+    filterGroup.addEventListener('click', function(event) {
+        if (!matchesSelector(event.target, 'a')) {
+          return;
+        }
+        var filterValue = event.target.getAttribute('data-filter');
+        iso.arrange({
+            filter: filterValue
+        });
+    });
+}
+
+var filterGroups = document.querySelectorAll('.jc-ln-list');
+for (var i = 0, len = filterGroups.length; i < len; i++) {
+  var filterGroup = filterGroups[i];
+  toggleFilter(filterGroup);
+}
+
+function toggleFilter(filterGroup) {
+  filterGroup.addEventListener('click', function(event) {
+    if (!matchesSelector(event.target, 'a')) {
+      return;
+    }
+    filterGroup.querySelector('.active').classList.remove('active');
+    event.target.classList.add('active');
   });
 }
-// var filtersElem = document.querySelector('.jc-ln-list');
-// filtersElem.addEventListener( 'click', function( event ) {
-//   if ( !apollo.hasClass( event.target, '.jc-ln-item')) {
-//     return;
-//   }
-//   var filterValue = event.target.getAttribute('data-filter');
-//   // use matching filter function
-//   iso.arrange({ filter: filterValue });
-// });
-
-// var filterGroups = document.querySelectorAll('.jc-ln-list');
-// for (var i = 0, len = filterGroups.length; i < len; i++) {
-//   var filterGroup = filterGroups[i];
-//   toggleFilter(filterGroup);
-// }
-//
-// function toggleFilter(filterGroup) {
-//   filterGroup.addEventListener('click', function(event) {
-//     if ( !matchesSelector( event.target, '.jc-ln-item' ) ) {
-//       return;
-//     }
-//     filterGroup.querySelector('.active').classList.remove('active');
-//     event.target.classList.add('active');
-//   });
-// }
